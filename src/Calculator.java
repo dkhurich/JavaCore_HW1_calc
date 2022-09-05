698905e2f730aa4d84e5363d50be8683a5fc0053
Denis Huric <dhuric231@gmail.com>
2022-09-05 14:23:18 +0300

    Add comment.

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,7 +6,9 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x != 0 && y != 0 ? (x / y) : 0; //(x, y) -> x / y
+    BinaryOperator<Integer> devide = (x, y) -> x != 0 && y != 0 ? (x / y) : 0;
+    /* (x, y) -> x / y Ошибка заключалась в том, что в коде не предусмотренна проверка деления на 0. Вариант исправления - добавить условие,
+    если делитель или делимое равен 0 -> возвращать 0.  */
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
